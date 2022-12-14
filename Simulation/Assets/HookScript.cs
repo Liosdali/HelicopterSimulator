dd8df1109003c3ac53244f0a751a1fa7eebe57e2
melihacil <knghtmellomello@gmail.com>
2022-12-14 16:43:02 +0300

    uPDATE

--- Simulation/Assets/HookScript.cs
+++ Simulation/Assets/HookScript.cs
@@ -7,7 +7,7 @@ public class HookScript : MonoBehaviour
 
 
     private Transform hookTransform = null;
-
+    private bool hook_Enabled = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,12 +22,37 @@ public class HookScript : MonoBehaviour
         {
             hookTransform.position = new Vector3(transform.position.x, transform.position.y -15, transform.position.z);
         }
+    }
 
-
-
+    public void HookFunction ()
+    {
+        Debug.Log(hook_Enabled);
+        hook_Enabled = true;
     }
 
 
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Deliverable"))
+        {
+            if (hook_Enabled)
+            {
+                hookTransform = other.GetComponentInParent<BoxCollider>().gameObject.transform;
+                Debug.Log(other.GetComponentInParent<BoxCollider>().gameObject.name);
+                other.gameObject.GetComponent<CapsuleCollider>().gameObject.SetActive(false);
+                //hookTransform = other.gameObject.transform;
+            }
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Deliverable"))
+            hook_Enabled = false;
+    }
+
+    /*
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Deliverable"))
@@ -36,4 +61,6 @@ public class HookScript : MonoBehaviour
             hookTransform = collision.gameObject.transform;
         }
     }
+
+    */
 }
