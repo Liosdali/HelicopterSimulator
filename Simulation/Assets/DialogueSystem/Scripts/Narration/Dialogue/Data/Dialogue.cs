e74e4b22442aca1be6a680b7a830f30f3f0d219f
melihacil <knghtmellomello@gmail.com>
2022-12-20 13:46:09 +0300

    Update Dialogue.cs

--- Simulation/Assets/DialogueSystem/Scripts/Narration/Dialogue/Data/Dialogue.cs
+++ Simulation/Assets/DialogueSystem/Scripts/Narration/Dialogue/Data/Dialogue.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Scriptable Objects/Narration/Dialogue/Dialogue")]
 public class Dialogue : ScriptableObject
 {
+
+    [SerializeField]
+    AudioSource audioSource;
+
+
     [SerializeField]
     private DialogueNode m_FirstNode;
     public DialogueNode FirstNode => m_FirstNode;
