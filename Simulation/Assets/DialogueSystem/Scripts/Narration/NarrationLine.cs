ed9c3770829b189115ea574020d122f0952f0381
melihacil <knghtmellomello@gmail.com>
2022-12-21 14:43:57 +0300

    Update dialogue

--- Simulation/Assets/DialogueSystem/Scripts/Narration/NarrationLine.cs
+++ Simulation/Assets/DialogueSystem/Scripts/Narration/NarrationLine.cs
@@ -5,9 +5,15 @@ public class NarrationLine : ScriptableObject
 {
     [SerializeField]
     private NarrationCharacter m_Speaker;
+
+    [SerializeField]
+    public AudioClip audioClip;
     [SerializeField]
     private string m_Text;
 
     public NarrationCharacter Speaker => m_Speaker;
     public string Text => m_Text;
+
+    private bool playing = false;
+    
 }
