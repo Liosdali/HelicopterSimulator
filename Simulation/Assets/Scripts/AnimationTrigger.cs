1452c765d87edc7124bb8ccac41d804e9bff931c
melihacil <knghtmellomello@gmail.com>
2022-12-22 15:05:02 +0300

    Update

--- Simulation/Assets/Scripts/AnimationTrigger.cs
+++ Simulation/Assets/Scripts/AnimationTrigger.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.XR.Interaction.Toolkit;
 public class AnimationTrigger : MonoBehaviour
 {
     [SerializeField] private Animator myDoor = null;
 
+    [SerializeField] private Transform pilotSeat;
+    [SerializeField] private GameObject pilot;
+    [SerializeField] private GameObject locomotionSystem;
 
+
+    private float waitSeconds = 3.0f;
+    private bool teleport = false;
+    private void Update()
+    {
+        if (teleport)
+        {
+            if (waitSeconds <= 0)
+            {
+                pilot.transform.position = pilotSeat.position;
+                pilot.transform.rotation = pilotSeat.rotation;
+                //Destroy(locomotionSystem.GetComponent<DeviceBasedContinuousMoveProvider>());
+                GetComponent<BoxCollider>().enabled = false;
+                Destroy(gameObject);
+            } 
+            else
+                waitSeconds -= Time.deltaTime;
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
 
         if (other.CompareTag("PlayerHand"))
         {
-           
+           teleport = true;
                 Debug.Log("Animasyon Baþladý");
             //   myDoor.Play("DoorOpen", 0, 0.0f); gameObject.SetActive(false);
             //  myDoor.SetBool("Door",true);
@@ -24,7 +46,8 @@ public class AnimationTrigger : MonoBehaviour
     {
         if (other.CompareTag("PlayerHand"))
         {
-
+            teleport = false;
+            waitSeconds = 3.0f;
             Debug.Log("Animasyon Bitti");
             //  myDoor.Play("DoorClose", 0, 0.0f); gameObject.SetActive(false);
             //myDoor.SetBool("Door",false);
