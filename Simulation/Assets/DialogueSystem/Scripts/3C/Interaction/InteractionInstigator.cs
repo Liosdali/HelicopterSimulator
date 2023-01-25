c5b7f614a51fd65f5d8ee77874a160cde7cb1ecf
Mete Baykal <mete.baykal@gazi.edu.tr>
2023-01-25 15:06:20 +0300

    Update InteractionInstigator.cs
    sd

--- Simulation/Assets/DialogueSystem/Scripts/3C/Interaction/InteractionInstigator.cs
+++ Simulation/Assets/DialogueSystem/Scripts/3C/Interaction/InteractionInstigator.cs
@@ -15,31 +15,24 @@ public class InteractionInstigator : MonoBehaviour
     {
         return m_NearbyInteractables.Count != 0;
     }
-
-    private void Update()
+    private void Start()
     {
-        /*
-        var rightHandDevices = new List<UnityEngine.XR.InputDevice>();
-        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.RightHand, rightHandDevices);
-        UnityEngine.XR.InputDevice device = rightHandDevices[0];
-        Debug.Log(string.Format("Device name '{0}' with role '{1}'", device.name, device.role.ToString()));
-        bool triggerValue;
-        */
-
         _trigger = inputActions.FindActionMap("XRI RightHand Interaction").FindAction("Activate");
         _trigger.Enable();
-
-     
+    }
+    private void Update()
+    {
+    
     }
     public void ToogleTrigger(InputAction.CallbackContext context)
     {
-        /* if (HasNearbyInteractables)
+        if (HasNearbyInteractables())
         {
             Debug.Log("Trigger button is pressed.");
             //Ideally, we'd want to find the best possible interaction (ex: by distance & orientation).
             m_NearbyInteractables[0].DoInteraction();
         }
-        */
+        
     }
 
     private void OnTriggerEnter(Collider other)
