7cfe559445319e873e7051253854e8c0faada6ba
melihacil <knghtmellomello@gmail.com>
2023-02-02 10:35:06 +0300

    Updating to the latest version

--- Simulation/Assets/Scripts/ArrowScript.cs
+++ Simulation/Assets/Scripts/ArrowScript.cs
@@ -6,59 +6,18 @@ public class ArrowScript : MonoBehaviour
 {
 
     [SerializeField]
-    private Transform _Target;
-    public Transform LookAtTarget { get { return _Target; } }
+    Transform m_Transform;
+    // Start is called before the first frame update
 
-
-    [SerializeField]
-    private Transform _Spinner;
-    public Transform Spinner { get { return _Spinner; } }
-
-    [SerializeField]
-    private Transform _Scaler;
-    public Transform Scaler { get { return _Scaler; } }
-
-    public static ArrowScript Instance;
-
-    public float _TurnSpeed;
-    public void SetTarget(Transform target = null)
-    {
-        _Target = target;
-    }
-    private void Awake()
-    {
-        // Singleton
-        Instance = this;
-    }
-
+    //Will use transfrom.look to implement mission arrow
     void Start()
     {
-        this.gameObject.SetActive(false);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (LookAtTarget)
-        {
-            transform.LookAt(LookAtTarget);
-        }
-        if (Spinner)
-        {
-            Spinner.transform.Rotate(0,0,_TurnSpeed * Time.deltaTime);
-        }
-    }
-
-
-    public void EndArrow ()
-    {
-        SetTarget(null);
-        this.gameObject.SetActive(false);
-    }
-
-    public void StartArrow(Transform target)
-    {
-        this.gameObject.SetActive(true);
-        SetTarget(target);
+        
     }
 }
