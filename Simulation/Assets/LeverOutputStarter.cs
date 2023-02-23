63e292b8ea1ec7ee93071e89a6ec4a753479dd25
melihacil <knghtmellomello@gmail.com>
2023-02-23 11:18:37 +0300

    Update LeverOutputStarter.cs

--- Simulation/Assets/LeverOutputStarter.cs
+++ Simulation/Assets/LeverOutputStarter.cs
@@ -2,25 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Oyedoyin.Common;
+
+
 public class LeverOutputStarter : MonoBehaviour
 {
 
 
 
     // ---------------------------- Events
-    public class InteractionEvent : UnityEvent { }
+    public UnityEvent onFlipOn = new UnityEvent();
+    public UnityEvent onFlipOff = new UnityEvent();
 
-    public InteractionEvent onFlipOn = new InteractionEvent();
-    public InteractionEvent onFlipOff = new InteractionEvent();
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private SilantroLever m_lever;
+
+
+    private bool m_Open;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (m_lever.leverOutput > 0.8f)
+        {
+            onFlipOn.Invoke();
+        }
+        else
+        {
+            onFlipOff.Invoke();
+        }
+
+
+        if (!m_Open)
+        {
+            OutputOnCheck();
+        }
+        else
+        {
+            OutputOffCheck();
+        }
+
+
+    }
+
+    private void OutputOffCheck()
+    {
+        if (m_lever.leverOutput < 0.78f)
+        {
+            m_Open = false;
+            onFlipOn.Invoke();
+        }
+    }
+    private void OutputOnCheck()
+    {
+        if (m_lever.leverOutput > 0.8f)
+        {
+            m_Open = true;
+            onFlipOn.Invoke();
+        }
     }
 }
