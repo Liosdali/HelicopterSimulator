71b292da0cc4098fc1a8c0d7f59b0c3dcfccf806
melihacil <knghtmellomello@gmail.com>
2023-03-04 20:55:34 +0300

    Update

--- Simulation/Assets/AltituteIndıcator.cs
+++ Simulation/Assets/AltituteIndıcator.cs
@@ -15,6 +15,18 @@ public class AltituteIndıcator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = new Quaternion(helicopter.rotation.x * -1 , helicopter.rotation.y, 0, 0);
+        //transform.rotation = new Quaternion(helicopter.rotation.x, helicopter.rotation.y, 0, 0);
+
+        //Vector3 eulerRotation = new Vector3(-helicopter.rotation.x, helicopter.rotation.y, helicopter.rotation.z);
+        //transform.rotation = Quaternion.Euler(eulerRotation);
+
+
+
+        Test();
+    }
+
+    void Test()
+    {
+        transform.LookAt(helicopter.transform);
     }
 }
