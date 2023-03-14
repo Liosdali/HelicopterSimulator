488bba673d749ff4fbd2531651369720aad45f4c
melihacil <knghtmellomello@gmail.com>
2023-03-14 15:38:21 +0300

    Update

---
+++ Simulation/Assets/testscri.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testscri : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.parent.GetComponentInChildren<BoxCollider>();
+    }
+
+}
