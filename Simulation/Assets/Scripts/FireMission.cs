ab7a2660e635dfeb590b533b840db008b5ceb9e7
Anıl Kapıkıran <52576317+anilmeister@users.noreply.github.com>
2023-03-28 15:47:45 +0300

    New quest improvements

---
+++ Simulation/Assets/Scripts/FireMission.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireMission : Mission
+{
+    [SerializeField]
+    private float waterPercentage;
+
+    [SerializeField]
+    private bool test;
+
+    [SerializeField]
+    private float flowRate;
+
+
+    private bool hook_enabled = true;
+
+    private bool trigger = false;
+
+    [SerializeField]
+    private ParticleSystemScripts particleSys;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (test)
+        {
+            if (waterPercentage > 0f)
+            {
+                waterPercentage -= 100f * Time.deltaTime * flowRate;
+                
+
+            }
+            particleSys.stopParticles();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (other.CompareTag("InitialPosition"))
+        {
+            if (hook_enabled)
+            {
+                if (waterPercentage<100f) { 
+                waterPercentage += waterPercentage *Time.deltaTime * flowRate;
+                Debug.Log(waterPercentage);
+                }
+                other.gameObject.GetComponent<CapsuleCollider>().gameObject.SetActive(false);
+
+            }
+        }
+        else if (other.CompareTag("FinalPosition"))
+        {
+            if (!trigger)
+            {
+                
+                trigger = true;
+                if (waterPercentage>0) { 
+                waterPercentage -= waterPercentage * Time.deltaTime * flowRate;
+                }
+                particleSys.stopParticles();
+
+
+            }
+        }
+    }
+}
--- Simulation/Assets/Scripts/ParticleSystemScripts.cs
+++ Simulation/Assets/Scripts/ParticleSystemScripts.cs
@@ -4,14 +4,7 @@ using UnityEngine;
 
 
 public class ParticleSystemScripts : MonoBehaviour
-
-
 {
-
-
-    [SerializeField]
-    private bool test;
-
     [SerializeField]
     private GameObject postSmoke;
 
@@ -27,33 +20,29 @@ public class ParticleSystemScripts : MonoBehaviour
         
     }
 
+    public void stopParticles()
+    {
+        for (int i = 0; i < ps.Length; i++)
+        {
+            var main = ps[i].main;
+            main.loop = false;
+            if (i == ps.Length - 1)
+            {
+                fireObject.SetActive(false);
+                postSmoke.SetActive(true);
+
+            }
+        }
+
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (test)
-        {
-            for(int i = 0; i < ps.Length; i++)
-            {
-                var main = ps[i].main;
-                main.loop = false;
-             if(i == ps.Length -1)
-                {
-                    fireObject.SetActive(false);
-                    postSmoke.SetActive(true);
-                    
-                }
-            }
-            /*foreach (ParticleSystem particle in ps)
-            {
-                var main = ps[particle].main;
-                main.loop = false;
-            }*/
-
-        }
 
         
     }
-
+    
 
 
 
