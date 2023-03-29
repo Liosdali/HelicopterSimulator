135f7d654b7244176e1b712361a372f6a11260a9
melihacil <knghtmellomello@gmail.com>
2023-03-29 12:58:03 +0300

    Update DisplayCams.cs

--- Simulation/Assets/DisplayCams.cs
+++ Simulation/Assets/DisplayCams.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class DisplayCams : MonoBehaviour
 {
+
+    [SerializeField]
+    private Camera[] cameras;
+    private int count = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+       foreach(Camera cam in cameras)
+        {
+            cam.gameObject.SetActive(false);
+        } 
+       cameras[0].gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown(KeyCode.Space.ToString()))
+        {
+            cameras[count].gameObject.SetActive(false);
+            count++;
+            if (count == cameras.Length)
+                count = 0;
+            cameras[count].gameObject.SetActive(true);
+        }
     }
 }
