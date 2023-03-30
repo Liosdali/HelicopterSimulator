70cbce84c29a8a30edc53cfc5c7201f95a6b3097
Mete Baykal <mete.baykal@gazi.edu.tr>
2023-03-30 14:37:57 +0300

    Gİt

--- Simulation/Assets/KeyAnimationScript.cs
+++ Simulation/Assets/KeyAnimationScript.cs
@@ -1,3 +1,4 @@
+using Oyedoyin.RotaryWing;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,28 @@ public class KeyAnimationScript : MonoBehaviour
 {
 
     public Animator anim;
+    public GameObject _helicopter;
+    public bool _oilButton = false;
+
+    
+
+    public void OilOpen()
+    {
+        _oilButton = !_oilButton;
+        Debug.Log(_oilButton);
+    }
 
 
     public void PlayKeyStartAnim()
     {
-        if(anim.GetBool("KeyStart") != true)
+        if(anim.GetBool("KeyStart") != true && _oilButton)
         {
+            _helicopter.GetComponent<RotaryController>().TurnOnEngines();
             anim.SetBool("KeyStart", true);
         }
         else
         {
+            _helicopter.GetComponent<RotaryController>().TurnOffEngines();
             anim.SetBool("KeyStart", false);
         }
     }
