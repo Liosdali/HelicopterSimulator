378653ead30cf36659209e28df37e4be1b5f503e
melihacil <knghtmellomello@gmail.com>
2023-03-30 13:18:49 +0300

    Update DisplayCams.cs

--- Simulation/Assets/DisplayCams.cs
+++ Simulation/Assets/DisplayCams.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Oyedoyin.Common;
 
 public class DisplayCams : MonoBehaviour
 {
 
     [SerializeField]
     private Camera[] cameras;
+
+    private GameObject m_attributeCanvas;
+
     private int count = 0;
     // Start is called before the first frame update
     void Start()
     {
+        m_attributeCanvas = GetComponentInChildren<SilantroMisc>().gameObject;
+        m_attributeCanvas.GetComponent<Canvas>().worldCamera = cameras[0];
        foreach(Camera cam in cameras)
         {
             cam.gameObject.SetActive(false);
@@ -28,6 +34,11 @@ public class DisplayCams : MonoBehaviour
             if (count == cameras.Length)
                 count = 0;
             cameras[count].gameObject.SetActive(true);
+            m_attributeCanvas.GetComponent<Canvas>().worldCamera = cameras[count];
+        }
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            m_attributeCanvas.SetActive(!m_attributeCanvas.activeSelf);
         }
     }
 }
