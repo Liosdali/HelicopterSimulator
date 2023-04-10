c4b6f7ca82f35c736a071d425999d232fe7441cf
melihacil <knghtmellomello@gmail.com>
2023-04-10 12:44:18 +0300

    Update

--- Simulation/Assets/Scripts/FadeScript.cs
+++ Simulation/Assets/Scripts/FadeScript.cs
@@ -35,6 +35,13 @@ public class FadeScript : MonoBehaviour
         }
     }
 
+    public void ChangeFadeStart()
+    {
+        _fadeOnStart = true;
+    }
+
+
+
     public void EndCutscene()
     {
         StartCoroutine(BlackCourutine());
