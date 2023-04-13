633df237c0dc71fb1e29e08fff42e8226944b1f5
melihacil <knghtmellomello@gmail.com>
2023-04-13 12:37:03 +0300

    Update VisibilityChanger.cs

--- Simulation/Assets/VisibilityChanger.cs
+++ Simulation/Assets/VisibilityChanger.cs
@@ -8,7 +8,7 @@ public class VisibilityChanger : MonoBehaviour
     [SerializeField]
     private GameObject m_player;
     [SerializeField]
-    private float m_scale = 0f;
+    private float m_range = 0f;
 
     private MeshRenderer m_renderer;
 
@@ -22,7 +22,7 @@ public class VisibilityChanger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, m_player.transform.position) < m_scale)
+        if (Vector3.Distance(transform.position, m_player.transform.position) < m_range)
             m_renderer.enabled = false;
         else
             m_renderer.enabled = true;
