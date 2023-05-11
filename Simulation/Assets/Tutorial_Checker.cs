5a69e8b548e465f4d6601814519d7e35ca1a2e39
melihacil <knghtmellomello@gmail.com>
2023-05-11 14:02:32 +0300

    Update

--- Simulation/Assets/Tutorial_Checker.cs
+++ Simulation/Assets/Tutorial_Checker.cs
@@ -101,10 +101,17 @@ public class Tutorial_Checker : MonoBehaviour
         m_DialogueController.FlipTutorial();
         m_LineRenderer.DeactivateLineRenderer();
         m_DialogueController.NextDialogueTuto();
-        m_dialogues[1].OpenDialoge();
-        Destroy(gameObject);
+        Invoke(nameof(EndInvoke), 1.5f);
+
 
         // change dialogue to the newer one 
+    }
+
+
+    private void EndInvoke()
+    {
+        m_dialogues[1].OpenDialoge();
+        Destroy(gameObject);
     }
 
 
