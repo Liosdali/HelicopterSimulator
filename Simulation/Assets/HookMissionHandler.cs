58b73767b0cbd6bd385c7add8e4a854dc95f8a78
Anıl Kapıkıran <52576317+anilmeister@users.noreply.github.com>
2023-05-11 16:03:05 +0300

    Update HookMissionHandler.cs

--- Simulation/Assets/HookMissionHandler.cs
+++ Simulation/Assets/HookMissionHandler.cs
@@ -77,6 +77,9 @@ public class HookMissionHandler : MonoBehaviour
         m_hookEnabled = !m_hookEnabled;
     }
 
+    private bool m_waterTake = false;
+    private bool m_waterFinish = false;
+
     // Sepetin calismasini saglayan kod blogu
     // Collision tabanli bir bicimde calisiyor
     private void OnTriggerStay(Collider other)
@@ -131,9 +134,13 @@ public class HookMissionHandler : MonoBehaviour
             {
                 if (m_waterPercentage < 100f)
                 {
-
-                    Invoke(nameof(FreezeHeliPos), 1f);
-
+                    if (!m_waterTake)
+                    {
+                        Invoke(nameof(FreezeHeliPos), 1f);
+                        m_Sounds[0].OpenDialoge();
+                        m_waterTake = true;
+                        m_waterFinish = true;
+                    }
                     //m_waterPercentage += m_waterPercentage * Time.deltaTime * m_flowRate;
                     m_waterPercentage += Time.deltaTime * m_flowRate;
                     m_waterSlider.value = m_waterPercentage * 0.01f;
@@ -141,14 +148,19 @@ public class HookMissionHandler : MonoBehaviour
 
                     m_waterSlider2.value = m_waterPercentage;
                     m_waterText2.text = "% " + ((int)m_waterPercentage).ToString();
-                    m_Sounds[0].OpenDialoge();
+                    
                     //Debug.Log(waterPercentage);
                 }
                 //other.gameObject.GetComponent<CapsuleCollider>().gameObject.SetActive(false);
                 else
                 {
-                    m_Sounds[1].OpenDialoge();
-                    m_HeliPos.UnlockHeliPos();
+                    if (m_waterFinish)
+                    {
+                        m_waterFinish = false;
+                        m_waterTake = false;
+                        m_Sounds[1].OpenDialoge();
+                        m_HeliPos.UnlockHeliPos();
+                    }
                 }
 
 
